a5ad8e7fe43c00a43a43cf56ea0402c3b4f585de
DESKTOP-H060KNS\JetBrains <rakhalib14@gmail.com>
2021-05-27 15:21:07 +0600

    excess code

--- src/main/java/com/apress/todo/todoclient/TodoClientApplication.java
+++ src/main/java/com/apress/todo/todoclient/TodoClientApplication.java
@@ -26,27 +26,22 @@ public class TodoClientApplication {
 			Iterable<ToDo> toDos = client.findAll();
 			assert toDos != null;
 			toDos.forEach(toDo -> log.info(toDo.toString()));
-			System.out.println("ALL TODOS DISPLAYED");
 
 			ToDo newToDo = client.upsert(
 					new ToDo("Drink plenty of Water daily!"));
 			assert newToDo != null;
 			log.info(newToDo.toString());
-			System.out.println("A NEW TODO INSERTED");
 
 			ToDo toDo = client.findById(newToDo.getId());
 			log.info(toDo.toString());
-			System.out.println("A NEW TODO FOUND");
 
 			ToDo completed = client.setCompleted(newToDo.getId());
 			assert completed.getCompleted();
 			log.info(completed.toString());
-			System.out.println("A NEW TODO COMPLETED");
 
 			client.delete(newToDo.getId());
 			assert client.findById(newToDo.getId()) == null;
 
-			System.out.println("A NEW TODO DELETED");
 		};
 	}
 
